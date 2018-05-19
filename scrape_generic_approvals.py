db37945978f6b6d2fcca16387e3fe8b8508a38c6
Marisa Carlos <mbcarlos96@gmail.com>
2018-05-18 22:00:45 -0400

    update

--- scrape_generic_approvals.py
+++ scrape_generic_approvals.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import xlsxwriter
 import time
+import string
+printable=set(string.printable)
+import csv
 import re
 from selenium import webdriver #This allows us to read the webpage if uses Javascript
 driver = webdriver.PhantomJS() #Use phantonJS as our webdriver, which runs in the background (e.g. doesn't open up a new window) and allows us to collect javascript items (which we might not need for this tutorial, but could be useful for you in the future)
@@ -11,7 +14,7 @@ def print_line():
     print " "
     
     
-month_list = ["January"] #,"February","March","April","May","June","July","August","September","October","November","December"]
+month_list = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 homepage_url = "http://wayback.archive-it.org/7993/20170111082703/http://www.fda.gov/Drugs/DevelopmentApprovalProcess/HowDrugsareDevelopedandApproved/DrugandBiologicApprovalReports/ANDAGenericDrugApprovals/ucm050527.htm"
 
@@ -22,7 +25,7 @@ homepage_soup = BeautifulSoup(driver.page_source,"lxml") #Feed the URL into beau
 #print homepage_soup.prettify() #Print the page source
 
 #for YYYY in range(2001,2015):
-for YYYY in [2002,2012]:
+for YYYY in [2001,2015]:
     print_line()
     print "YEAR =",YYYY
     #year_tag = homepage_soup.find_all('a',href=True,text="%s"%(YYYY))
@@ -53,7 +56,7 @@ for YYYY in [2002,2012]:
             month_tag = year_soup.find_all('a',href=True,text=re.compile(month))[first_generic_col]
             print "Month tag for ",month,"is:"
             print month_tag
-            month_link = month_tag.get('href')
+            month_link = "http://wayback.archive-it.org" + month_tag.get('href')
             print "Month URL is:", month_link
     
         elif YYYY>2006:
@@ -61,7 +64,7 @@ for YYYY in [2002,2012]:
             #print year_soup.prettify()
             
             month_tag = year_soup.find('a',href=True,text=re.compile(month))
-            month_link = month_tag.get('href')
+            month_link = "http://wayback.archive-it.org" + month_tag.get('href')
             print "Month URL is:", month_link
             print "NUMBER OF TAGS WITH MONTH IN THE NAME:",len(year_soup.find_all('a',href=True,text=re.compile(month)))
     
@@ -69,7 +72,27 @@ for YYYY in [2002,2012]:
         driver.get(month_link)
         month_soup = BeautifulSoup(driver.page_source,"lxml")
         
-        print month_soup.prettify()
+        #print month_soup.prettify()
+        
+        #Pull all of the table elements out of the table and store them in a csv file named year_month
+        csv_filename = str(YYYY)+month+".csv"
+        with open(csv_filename,'w') as csv_file:
+            
+            #Pull out all the TR tags (table rows), and then take each TH (header) OR TD (table data) and write as a new cell to CSV file (remove commas to make easier)
+            for tr_tag in month_soup.find_all('tr'):
+                print_line()
+                #print tr_tag.prettify()
+                
+                for cell in tr_tag.find_all(['th','td']):
+                    print cell.get_text().strip()
+                    #cell_value_unencoded = cell.get_text().replace(",","")
+                    #cell_value = cell_value_unencoded.encode('utf-8')
+                    
+                    cell_value = cell.get_text().replace(",","")
+                    cell_value = filter(lambda x: x in printable, cell_value)
+                    
+                    csv_file.write(cell_value+",")
+                csv_file.write("\n")
     
 driver.quit() #Close the driver
     
